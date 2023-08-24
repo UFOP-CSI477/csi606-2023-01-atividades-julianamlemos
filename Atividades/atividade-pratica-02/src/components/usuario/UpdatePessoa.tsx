4257cbec4be54ee821ec0b8823527dad8ac669ad
Juliana M. Lemos <42423350+julianamlemos@users.noreply.github.com>
2023-08-23 22:44:01 -0300

    Add files via upload

---
+++ Atividades/atividade-pratica-02/src/components/usuario/UpdatePessoa.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { CidadeInterface } from "../cidades/ListCidades";
+
+const UpdatePessoa = () => {
+    
+    const navigate = useNavigate();
+
+    const [nome, setNome] = useState('');
+    const [cidadeId, setCidadeId] = useState(0);
+    const [ cidades, setCidades ] = useState<CidadeInterface[]>([]);
+    const [ tipos, setTipos ] = useState<TipoInterface[]>([]);
+ 
+    const { id } = useParams();
+
+    useEffect(() => {
+
+        api.get('/cidades')
+            .then(response => {
+                setCidades(response.data);
+            })
+
+        api.get('/tipos_sanguineos')
+            .then(response => {
+                setTipos(response.data);
+            })
+
+    },[])
+
+    useEffect(() => {
+
+        api.get(`/pessoas/${id}`)
+            .then(response => {
+                setNome(response.data.nome);
+                setCidadeId(response.data.cidade_id);
+                setTipoId(response.data.cidade_id);
+            })
+
+    }, [id]);
+
+    const handleUpdatePessoa = async (event : React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+
+        const data = {
+            nome,
+            cidade_id: cidadeId,
+        }
+
+        try {
+            
+            await api.put('/pessoas', data);
+            alert('Pessoa atualizada com sucesso');
+            navigate('/pessoas');
+
+        } catch (error) {
+            alert('Erro ao atualizar Pessoa!');
+            console.error(error);
+        }
+
+    }
+
+    return(
+
+        <div>
+            <h3>Atualização de Pessoa: {nome}</h3>
+
+            <form onSubmit={handleUpdatePessoa}>
+
+            <div>
+                    <label htmlFor="nome">Nome</label>
+                    <input type="text"
+                        name="nome"
+                        id="nome"
+                        value={nome}
+                        placeholder="Nome da pessoa"
+                        onChange={e => setNome(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="cidade">Cidade</label>
+
+                    <select 
+                        name="cidade" 
+                        id="cidade"
+                        value={cidadeId}
+                        onChange={e => 
+                            setCidadeId(parseInt(e.target.value))
+                        }
+                        >
+
+                        <option 
+                            value="0" 
+                            selected>Selecione:</option>
+
+                        {
+                            cidades.map(cidade => (
+                                <option
+                                    value={cidade.id}>{cidade.nome}</option>
+                            ))
+                        }
+
+                    </select>
+
+                </div>
+
+                <button type="submit">Atualizar</button>
+                <button type="reset">Limpar</button>
+                <Link to="/pessoas">Voltar</Link>
+            </form>
+        </div>
+
+    );
+
+
+
+}
+
+export default UpdatePessoa;
